6ba0af5e5e55daddc9c71b0f719d3a14e6c0da51
athomasprojects <amanda.thomas14@gmail.com>
2024-11-18 15:46:25 -0500

    define token types

---
+++ src/test_token.zig
@@ -0,0 +1,15 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const token = @import("token.zig");
+const Token = token.Token;
+
+test "identifiers" {
+    const keys = [_][]const u8{ "let", "fn", "if", "else", "true", "false", "return", "does not exist!" };
+    for (keys) |key| {
+        switch (token.lookupIdent(key)) {
+            .Let, .Function, .If, .Else, .True, .False, .Return => |_| try std.testing.expect(true),
+            .Ident => |ident| try std.testing.expect(std.mem.eql(u8, ident, key)),
+            else => unreachable,
+        }
+    }
+}
---
+++ src/token.zig
@@ -0,0 +1,54 @@
+// The Lexer generates tokens from an input stream of bytes
+
+const std = @import("std");
+
+const StaticStringMap = std.static_string_map.StaticStringMap;
+
+pub const Token = union(enum) {
+    // Identifiers
+    Illegal: void,
+    Eof: void,
+    Integer: i32,
+    Ident: []const u8,
+    String: []const u8,
+    Assign: void,
+    Plus: void,
+    Minus: void,
+    Equal: void,
+    NotEqual: void,
+    Bang: void,
+    Asterisk: void,
+    Slash: void,
+    // Delimiters
+    LeftParen: void,
+    RightParen: void,
+    LeftBrace: void,
+    RightBrace: void,
+    LeftBracket: void,
+    RightBracket: void,
+    LessThan: void,
+    Comma: void,
+    Semicolon: void,
+    // Keywords
+    Let: void,
+    Function: void,
+    If: void,
+    Else: void,
+    True: void,
+    False: void,
+    Return: void,
+};
+
+const IdentMap = StaticStringMap(Token).initComptime(.{
+    .{ "let", .Let },
+    .{ "fn", .Function },
+    .{ "if", .If },
+    .{ "else", .Else },
+    .{ "true", .True },
+    .{ "false", .False },
+    .{ "return", .Return },
+});
+
+pub fn lookupIdent(str: []const u8) Token {
+    return IdentMap.get(str) orelse Token{ .Ident = str };
+}
