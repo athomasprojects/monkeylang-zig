b3853b31289a057cb2223b46b06b3b95ab72de56
athomasprojects <amanda.thomas14@gmail.com>
2024-11-18 22:36:21 -0500

    advance lexer and skip whitespace

--- src/lexer.zig
+++ src/lexer.zig
@@ -7,11 +7,13 @@ pub const Lexer = struct {
     input: []const u8,
     position: usize = 0,
     ch: ?u8 = null,
-    length: usize = undefined,
+    length: usize = 0,
 
     pub fn init(input: []const u8) Lexer {
         return switch (input.len) {
-            0 => Lexer{ .input = input, .length = input.len },
+            0 => Lexer{
+                .input = input,
+            },
             else => Lexer{ .input = input, .ch = input[0], .length = input.len },
         };
     }
@@ -22,11 +24,44 @@ pub const Lexer = struct {
         std.debug.print("    input: \"{s}\",\n", .{self.input});
         std.debug.print("    position: {},\n", .{self.position});
         if (self.ch) |ch| {
-            std.debug.print("    ch: {c},\n", .{ch});
+            std.debug.print("    ch: '{c}',\n", .{ch});
         } else {
             std.debug.print("    ch: null,\n", .{});
         }
         std.debug.print("    length: {},\n", .{self.length});
         std.debug.print("}}\n", .{});
+    }
+
+    pub fn advance(self: *Lexer) void {
+        if (self.length > 0) {
+            if (self.position >= self.length - 1) {
+                self.ch = null;
+            } else {
+                self.position += 1;
+                self.ch = self.input[self.position];
+            }
+        }
+    }
+
+    fn is_string(ch: u8) bool {
+        return ch == '"';
+    }
+
+    fn is_letter(ch: u8) bool {
+        return std.ascii.isAlphabetic(ch) or ch == '_';
+    }
+
+    fn scan_until(self: *Lexer, condition: fn (ch: u8) bool) void {
+        while (self.ch) |ch| {
+            if (condition(ch)) {
+                self.advance();
+            } else {
+                break;
+            }
+        }
+    }
+
+    pub fn skip_whitespace(self: *Lexer) void {
+        self.scan_until(std.ascii.isWhitespace);
     }
 };
--- src/test_lexer.zig
+++ src/test_lexer.zig
@@ -2,31 +2,66 @@ const std = @import("std");
 const expect = std.testing.expect;
 const Lexer = @import("lexer.zig").Lexer;
 
+const input =
+    \\let five = 5;
+    \\let ten = 10;
+    \\let add = fn(x, y){
+    \\x + y;
+    \\};
+    \\let result = add(five, ten);
+    \\if x != y {
+    \\let foo_bar = y / x * 69;
+    \\return true;
+    \\} else {
+    \\return false;
+    \\}
+    \\10 == 10;
+    \\10 != 9;
+    \\baz =!= 420
+    \\"string"
+;
+
 test "init lexer" {
-    const input =
-        \\let five = 5;
-        \\let ten = 10;
-        \\let add = fn(x, y){
-        \\x + y;
-        \\};
-        \\let result = add(five, ten);
-        \\if x != y {
-        \\let foo_bar = y / x * 69;
-        \\return true;
-        \\} else {
-        \\return false;
-        \\}
-        \\10 == 10;
-        \\10 != 9;
-        \\baz =!= 420
-        \\"string"
-    ;
     const lexers = [_]Lexer{
         Lexer.init(""), Lexer.init(input),
     };
-    const vals = [_]Lexer{ .{ .input = "", .position = 0, .ch = null, .length = 0 }, .{ .input = input, .position = 0, .ch = input[0], .length = input.len } };
+    const vals = [_]Lexer{ .{
+        .input = "",
+        .position = 0,
+        .ch = null,
+    }, .{ .input = input, .position = 0, .ch = input[0], .length = input.len } };
     for (lexers, vals) |lexer, val| {
-        lexer.debugPrint();
+        // lexer.debugPrint();
         try expect(std.meta.eql(lexer, val));
     }
+}
+
+test "advance lexer" {
+    var lexer1 = Lexer.init("");
+    var lexer2 = Lexer.init(input);
+
+    try expect(std.meta.eql(lexer1, Lexer{ .input = "" }));
+    lexer1.advance();
+    try expect(std.meta.eql(lexer1, Lexer{
+        .input = "",
+    }));
+
+    // lexer1.debugPrint();
+    // lexer2.debugPrint();
+    try expect(std.meta.eql(lexer2, Lexer{ .input = input, .position = 0, .ch = input[0], .length = input.len }));
+    lexer2.advance();
+    try expect(std.meta.eql(lexer2, Lexer{ .input = input, .position = 1, .ch = input[1], .length = input.len }));
+    // lexer2.debugPrint();
+}
+
+test "skip whitespace" {
+    var lexer1 = Lexer.init("");
+    const str = "   hello";
+    var lexer2 = Lexer.init(str);
+
+    lexer1.skip_whitespace();
+    lexer2.skip_whitespace();
+
+    try expect(std.meta.eql(lexer1, Lexer{ .input = "" }));
+    try expect(std.meta.eql(lexer2, Lexer{ .input = str, .position = 3, .ch = 'h', .length = str.len }));
 }
