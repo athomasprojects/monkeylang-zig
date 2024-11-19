df916c2e79edddeaaa4f0b69e0b2c9c6ed7680b2
athomasprojects <amanda.thomas14@gmail.com>
2024-11-19 17:05:56 -0500

    add REPL

---
+++ src/repl.zig
@@ -0,0 +1,43 @@
+// Monkeylang REPL
+
+const std = @import("std");
+const Lexer = @import("lexer.zig").Lexer;
+const Allocator = std.mem.Allocator;
+const stdout = std.io.getStdOut().writer();
+const stdin = std.io.getStdIn().reader();
+
+const prompt = ">> ";
+
+pub fn start() void {
+    stdout.print("Hello! This is the monkey programming language!\nFeel free to type in commands.\n", .{}) catch {};
+    loop();
+    return;
+}
+
+fn loop() void {
+    var buf: [1000]u8 = undefined;
+
+    stdout.print("{s}", .{prompt}) catch {};
+
+    if (stdin.readUntilDelimiterOrEof(&buf, '\n')) |input| {
+        if (input) |str| {
+            if (condition(str)) {
+                var lexer = Lexer.init(str);
+                while (lexer.nextToken()) |next_tok| {
+                    next_tok.debugPrint();
+                }
+                loop();
+            }
+        }
+    } else |_| {}
+}
+
+fn condition(input: []const u8) bool {
+    // exit REPL if 'quit' command is entered
+    // else, keep accepting user input
+    if (std.mem.eql(u8, input, "quit")) {
+        return false;
+    } else {
+        return input.len >= 0;
+    }
+}
